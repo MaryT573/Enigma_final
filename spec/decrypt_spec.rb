0cf3d463cda607ce856267f4f6d3fdec949cabed
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-06-12 13:40:54 -0600

    more tests

--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -5,8 +5,10 @@ require './lib/decrypt'
 RSpec.describe Decrypt do
   before(:each) do
     @decrypt = Decrypt.new
+    @encrypt = Encrypt.new
     @message = "Hello World"
     @message2 = "Hello World!"
+    @encrypted = @encrypt.encrypt("hello world", "02715", "040895")
   end
 
   it 'exists' do
@@ -18,6 +20,10 @@ RSpec.describe Decrypt do
     expect(@decrypt.decrypt("keder ohulw", "02715", "040895")).to eq("hello world")
   end
 
+  xit 'can decrypt message correctly with only key and no date' do
+    expect(@decrypt.decrypt(@encrypted, "02715")).to be_a(String)
+    expect(@decrypt.decrypt(@encrypted, "02715")).to eq("hello world")
+  end
 
   it 'decrypt ignores punctuation' do
     expect(@decrypt.decrypt("keder ohulw!", "02715", "040895")).to eq("hello world!")
