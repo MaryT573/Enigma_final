c50fbf41350f09a8c5dc5b8064ff52fe202f0c0f
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-06-12 19:11:48 -0600

    create encrypt and decrypt methods in enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,32 @@
 require 'time'
 require_relative 'keys_offsets'
-require_relative 'rotatable'
 require_relative 'encrypt'
 require_relative 'decrypt'
 
 class Enigma
-  include Rotatable
+  attr_accessor :encrypt, :decrypt
 
-def initialize
-  @argument = argument
+  def initialize
+    super
+    @encrypt = Encrypt.new
+    @decrypt = Decrypt.new
+  end
+
+  def encrypt(message, key = @key, date = @offset)
+    encryption = @encrypt.encrypt(message, key, date)
+    encrypt_hash = {
+      :encryption => encryption,
+      :key => key,
+      :date => date
+    }
+  end
+
+  def decrypt(message, key, date = @date)
+    decryption = @decrypt.decrypt(message, key, date)
+    decrypt_hash = {
+      :encryption => decryption,
+      :key => key,
+      :date => date
+    }
+  end
 end
