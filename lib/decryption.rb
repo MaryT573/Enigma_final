b4ac151e46b8f323da565ef0dd1e2a30c1ba9d94
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-06-14 13:47:33 -0600

    Refactor encryption and decryption runner files

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -1,20 +1,12 @@
-require 'time'
-require_relative 'keys_offsets'
-require_relative 'rotatable'
 require_relative 'decrypt'
-
-class Decryption
-  attr_accessor :decryption
-  @decryption = Decrypt.new
-  start = File.open(ARGV[0], "r")
-  encrypted_text = start.read
-  encrypted_text = encrypted_text.strip
-  start.close
-  writer = File.open(ARGV[1], "w")
-  key = ARGV[2]
-  date = ARGV[3]
-  decrypted_text = @decryption.decrypt(encrypted_text, key, date)
-  writer.write(decrypted_text)
-  writer.close
-  puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
-end
+@decryption = Decrypt.new
+start = File.open(ARGV[0], "r")
+encrypted_text = start.read
+start.close
+writer = File.open(ARGV[1], "w")
+key = ARGV[2]
+date = ARGV[3]
+decrypted_text = @decryption.decrypt(encrypted_text, key, date)
+writer.write(decrypted_text)
+writer.close
+puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
