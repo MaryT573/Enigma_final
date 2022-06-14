34415207d31b40f611935e5164c19f4f2abec764
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-06-14 17:29:48 -0600

    small changes to encryption

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,9 +2,11 @@ require_relative 'encrypt'
 @encryption = Encrypt.new
 start = File.open(ARGV[0], "r")
 incoming_text = start.read
-changed_text = @encryption.encrypt(incoming_text, "82648", "240818")
+date = "240818"
+key = "82648"
+changed_text = @encryption.encrypt(incoming_text, key, date)
 start.close
 writer = File.open(ARGV[1], "w")
 writer.write(changed_text)
 writer.close
-puts "Created #{ARGV[1]} with the key 82648 and date 240818"
+puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
